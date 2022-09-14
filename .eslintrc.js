f41a890e4d2c4184f608786f428c27447a9b3164
GeoS74 <gsirotkin@yandex.ru>
2022-09-14 18:52:05 +0500

    added tsconfig and eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "airbnb-base",
+        "airbnb-typescript/base"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+      "project": './tsconfig.json'
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+      'no-param-reassign': 'off',
+      'class-methods-use-this': 'off',
+      'no-restricted-syntax': 'off',
+      'import/prefer-default-export': 'off',
+    }
+}
